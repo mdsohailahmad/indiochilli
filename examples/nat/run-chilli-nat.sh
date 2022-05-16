20a39092988c56fe42ef6e13aaf9c9b7d7c89b2b
Sohail Ahmad <sohail.ahmad@wifi-soft.com>
2022-05-16 14:09:55 +0000

    Added config files used for testing of NAT and Bridge mode

---
+++ examples/nat/run-chilli-nat.sh
@@ -0,0 +1,9 @@
+cp wireless /etc/config/wireless
+cp network /etc/config/network
+cp firewall /etc/config/firewall
+cp defaults /etc/chilli/defaults
+cp indiochilli /etc/config/indiochilli
+cp firewall.user /etc/firewall.user
+uci set firewall.@zone[0].masq='1'
+uci commit firewall
+wifi
